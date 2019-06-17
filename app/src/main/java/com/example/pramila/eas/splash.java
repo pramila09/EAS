ae14ddd58aab324f2630b4fb8a0a3a75c0c24fe0
pramila09 <pramila.shawol@gmail.com>
2019-06-17 19:45:41 +0545

    Initial commit

---
+++ app/src/main/java/com/example/pramila/eas/splash.java
@@ -0,0 +1,39 @@
+package com.example.pramila.eas;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.TextView;
+
+public class splash extends AppCompatActivity {
+    private TextView tv;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+        tv = (TextView)findViewById(R.id.tv);
+        Animation myanim = AnimationUtils.loadAnimation(this, R.anim.mytransition);
+        tv.startAnimation(myanim);
+        tv.startAnimation(myanim);
+
+        final Intent i= new Intent(this,MainActivity.class);
+        Thread timer = new Thread() {
+            public void run() {
+                try {
+                    sleep(5000);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                } finally {
+                    startActivity(i);
+                    finish();
+                }
+            }
+        };
+        timer.start();
+
+
+    }
+}
