b4f6ca85bb43b7fc86139f87d6c3f62903d2d7a4
pramila09 <pramila.shawol@gmail.com>
2019-06-28 20:25:26 +0545

    tried to connect to database

--- app/src/main/java/com/example/pramila/eas/MainActivity.java
+++ app/src/main/java/com/example/pramila/eas/MainActivity.java
@@ -2,9 +2,11 @@ package com.example.pramila.eas;
 
 import android.app.ActionBar;
 import android.app.Activity;
+import android.app.ProgressDialog;
 import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
+import android.net.Uri;
 import android.os.AsyncTask;
 import android.support.design.widget.BottomNavigationView;
 import android.support.v7.app.AppCompatActivity;
@@ -17,47 +19,45 @@ import android.widget.Toast;
 import org.json.JSONException;
 import org.json.JSONObject;
 
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
 import java.io.IOException;
 import java.io.InputStream;
+import java.io.InputStreamReader;
 import java.io.OutputStream;
+import java.io.OutputStreamWriter;
 import java.net.HttpURLConnection;
 import java.net.MalformedURLException;
 import java.net.URL;
 
 public class MainActivity extends AppCompatActivity {
 
-    Button btn;
-    Context context;
-    String NAME, PASSWORD;
-    String NAme = null, PAssword = null;
-    int tmp;
-    SharedPreferences preference;
+    public static final int CONNECTION_TIMEOUT = 10000;
+    public static final int READ_TIMEOUT = 15000;
+
+    private EditText etemail, etpassword;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        final EditText editText = (EditText)findViewById(R.id.Edittextemail);
-        EditText editText1 =(EditText)findViewById(R.id.Edittextpassword);
+        // final EditText editText = (EditText)findViewById(R.id.Edittextemail);
+        // EditText editText1 =(EditText)findViewById(R.id.Edittextpassword);
 
-        final EditText Edittextemail = (EditText)findViewById(R.id.Edittextemail);
-        final EditText Edittextpassword =(EditText)findViewById(R.id.Edittextpassword);
-
-        preference = this.getSharedPreferences("MYPREFS", Context.MODE_PRIVATE);
-
-
+        final EditText etemail = (EditText) findViewById(R.id.email);
+        final EditText etpassword = (EditText) findViewById(R.id.password);
 
 
         Button btn = (Button) findViewById(R.id.btn);
         btn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                if(Edittextemail.getText().length()<=0)
+                if(etemail.getText().length()<=0)
                 {
                     Toast.makeText(MainActivity.this, "Enter email address", Toast.LENGTH_SHORT).show();
                 }
 
-                else if(Edittextpassword.getText().length()<=0)
+                else if(etpassword.getText().length()<=0)
                 {
                     Toast.makeText(MainActivity.this, "Enter password", Toast.LENGTH_SHORT).show();
                 }else {
@@ -75,78 +75,145 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
-
-
-
-       // BackGround b = new BackGround();
-        //b.execute(type, EMAIL, PASSWORD);
-
-
-
     }
 
+    public void checkLogin(View arg0) {
+        final String email = etemail.getText().toString();
+        final String password = etpassword.getText().toString();
+
+        new AsyncLogin().execute(email, password);
     }
-   /* class BackGround extends AsyncTask<String, String, String> {
+
+    private class AsyncLogin extends AsyncTask<String, String, String> {
+        ProgressDialog pdLoading = new ProgressDialog(MainActivity.this);
+        HttpURLConnection conn;
+        URL url = null;
 
         @Override
         protected String doInBackground(String... params) {
-            String name = params[0];
-            String password = params[1];
-            String data="";
-            int tmp;
+            try {
+
+                // Enter URL address where your php file resides
+                url = new URL("http://10.0.2.2/EmpAdmin/login.php");
+
+            } catch (MalformedURLException e) {
+                // TODO Auto-generated catch block
+                e.printStackTrace();
+                return "exception";
+            }
+            try {
+                // Setup HttpURLConnection class to send and receive data from php and mysql
+                conn = (HttpURLConnection) url.openConnection();
+                conn.setReadTimeout(READ_TIMEOUT);
+                conn.setConnectTimeout(CONNECTION_TIMEOUT);
+                conn.setRequestMethod("POST");
+
+                // setDoInput and setDoOutput method depict handling of both send and receive
+                conn.setDoInput(true);
+                conn.setDoOutput(true);
+
+                // Append parameters to URL
+                Uri.Builder builder = new Uri.Builder()
+                        .appendQueryParameter("username", params[0])
+                        .appendQueryParameter("password", params[1]);
+                String query = builder.build().getEncodedQuery();
+
+                // Open connection for sending data
+                OutputStream os = conn.getOutputStream();
+                BufferedWriter writer = new BufferedWriter(
+                        new OutputStreamWriter(os, "UTF-8"));
+                writer.write(query);
+                writer.flush();
+                writer.close();
+                os.close();
+                conn.connect();
+
+            } catch (IOException e1) {
+                // TODO Auto-generated catch block
+                e1.printStackTrace();
+                return "exception";
+            }
 
             try {
-                URL url = new URL("http://localhost:8080/login.php");
-                String urlParams = "username="+name+"&password="+password;
 
-                HttpURLConnection httpURLConnection = (HttpURLConnection) url.openConnection();
-                httpURLConnection.setDoOutput(true);
-                OutputStream os = httpURLConnection.getOutputStream();
-                os.write(urlParams.getBytes());
-                os.flush();
-                os.close();
+                int response_code = conn.getResponseCode();
 
-                InputStream is = httpURLConnection.getInputStream();
-                while((tmp=is.read())!=-1){
-                    data+= (char)tmp;
+                // Check if successful connection made
+                if (response_code == HttpURLConnection.HTTP_OK) {
+
+                    // Read data sent from server
+                    InputStream input = conn.getInputStream();
+                    BufferedReader reader = new BufferedReader(new InputStreamReader(input));
+                    StringBuilder result = new StringBuilder();
+                    String line;
+
+                    while ((line = reader.readLine()) != null) {
+                        result.append(line);
+                    }
+
+                    // Pass data to onPostExecute method
+                    return (result.toString());
+
+                } else {
+
+                    return ("unsuccessful");
                 }
 
-                is.close();
-                httpURLConnection.disconnect();
-
-                return data;
-            } catch (MalformedURLException e) {
-                e.printStackTrace();
-                return "Exception: "+e.getMessage();
             } catch (IOException e) {
                 e.printStackTrace();
-                return "Exception: "+e.getMessage();
+                return "exception";
+            } finally {
+                conn.disconnect();
+            }
+
+
+        }
+
+        @Override
+        protected void onPostExecute(String result) {
+            pdLoading.dismiss();
+
+            if (result.equalsIgnoreCase("true")) {
+                /* Here launching another activity when login successful. If you persist login state
+                use sharedPreferences of Android. and logout button to clear sharedPreferences.
+                 */
+
+                Intent intent = new Intent(MainActivity.this, homepage.class);
+                startActivity(intent);
+                MainActivity.this.finish();
+
+            } else if (result.equalsIgnoreCase("false")) {
+
+                // If username and password does not match display a error message
+                Toast.makeText(MainActivity.this, "Invalid email or password", Toast.LENGTH_LONG).show();
+
+            } else if (result.equalsIgnoreCase("exception") || result.equalsIgnoreCase("unsuccessful")) {
+
+                Toast.makeText(MainActivity.this, "OOPs! Something went wrong. Connection Problem.", Toast.LENGTH_LONG).show();
+
             }
         }
 
         @Override
-        protected void onPostExecute(String s) {
-            String err=null;
-            try {
-                JSONObject root = new JSONObject(s);
-                JSONObject tblemployee = root.getJSONObject("tblemployee");
-                NAme = tblemployee.getString("username");
-                PAssword = tblemployee.getString("password");
+        protected void onPreExecute() {
+            super.onPreExecute();
 
-            } catch (JSONException e) {
-                e.printStackTrace();
-                err = "Exception: "+e.getMessage();
-            }
-
-            Intent i = new Intent(context, homepage.class);
-            i.putExtra("name", NAme);
-            i.putExtra("password", PAssword);
-
-            i.putExtra("err", err);
-            startActivity(i);
+            //this method will be running on UI thread
+            pdLoading.setMessage("\tLoading...");
+            pdLoading.setCancelable(false);
+            pdLoading.show();
 
         }
     }
 }
-*/
+
+
+
+
+
+
+
+
+
+
 
